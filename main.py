867f0b96e044eabbf217ea0a770c063a3f37970c
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2021-11-29 18:38:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#retengle formula
+ 
+wigth = input("AI :Enter wigth ")
+wigth =  int(wigth)
+Length = input("Ai: Enter length ")
+Length = int(Length)
+
+finalarea = Length * wigth
+Text= "Your Area is %s "
+print( Text % finalarea )
